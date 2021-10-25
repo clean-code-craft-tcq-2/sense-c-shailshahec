e6f99c3710a8971976be9763224720a6edc78ae6
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 19:09:52 +0530

    Update stats.c
    Added logic for average, minimum, maximum

--- stats.c
+++ stats.c
@@ -7,26 +7,57 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.max = 0;
     int loopcount = 0, sum = 0;
     
- for (loopcount = 0; loopcount < setlength; loopcount++)
- {
-     if(numberset[loopcount] > numberset[loopcount+1])
-     {
-         s.max = numberset[loopcount];
-     }
-     if(numberset[loopcount] < numberset[loopcount+1])
-     {
-         s.min =  numberset[loopcount];
-     }
-     
-      sum = sum + numberset[loopcount];
- }
-    
-    s.average = sum / setlength;
-    
+    s.max =  get_arrmax(numberset,setlength);
+    s.min = get_arrmin (numberset,setlength);
+    s.average = get_arraver(numberset,setlength);
+
     return s;
 }
 
+float get_arrmax(float a[], int num_elements)
+{
+  int i, max;
+   max = a[0];
+   for (i=1; i<num_elements; i++)
+   {
+      if (a[i]>max)
+      {
+         max=a[i];
+      }
+   }
 
+   return(max);
+}
+
+float get_arrmin(float a[], int num_elements)
+{
+   int i, min;
+   min = a[0];
+   for (i=1; i<num_elements; i++)
+   {
+      if (a[i]<min)
+      {
+         min=a[i];
+      }
+   }
+
+   return(min);
+}
+
+float get_aver(float a[], int num_elements)
+{ 
+ int sum,i;
+   float avg;
+   sum=0;
+   avg=0;
+
+   for (i=0; i<num_elements;i++)
+   {
+      sum=sum+a[i];
+      avg=(float)sum/(i+1);
+   }
+   return(avg);
+}
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
