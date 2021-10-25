25a5e23bc5ad454aacf8e430bcb7b35579b2eeb4
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 20:16:40 +0530

    Update sense_alert.c
    added the function header

--- sense_alert.c
+++ sense_alert.c
@@ -1,11 +1,47 @@
 #include "sense_alert.h"
 
-void emailAlerter()
+/***************************************************************************************************
+*                                                                                                  *
+*  FILENAME:                                                                                      */
+/**\file       sense_alert.c
+*
+*  \brief       Source file for selecting the senser alert
+
+*
+* FILE VERSION INFORMATION:  $Revision: $ 1.0.0
+*                            $Date:     $ 25 oct 2021
+*                            $Author:   $
+*
+* REVISION HISTORY: V1.0
+*
+***************************************************************************************************/
+
+//**************************************************************************************************
+// FUNCTION:           emailAlerter
+//
+//! \brief             function to increment emailAlerter counter
+//!
+//! //! \param[in]     none		            
+//! //! \param[in]     none		         
+//! \return            none
+//
+//************************************************************************************************** 
+void emailAlerter(void)
 {
     emailAlertCallCount++;
 }
 
-void ledAlerter()
+//**************************************************************************************************
+// FUNCTION:           ledAlerter
+//
+//! \brief             function to increment ledAlerter counter
+//!
+//! //! \param[in]     none		            
+//! //! \param[in]     none		         
+//! \return            none
+//
+//************************************************************************************************** 
+void ledAlerter(void)
 {
    ledAlertCallCount++;
 }
