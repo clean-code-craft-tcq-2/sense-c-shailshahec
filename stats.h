15ab1171da2af746797cfb42bac64e84108e2dcc
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 19:10:50 +0530

    Update stats.h
    added header for average, minimum, maximum

--- stats.h
+++ stats.h
@@ -4,5 +4,9 @@ struct Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
+float get_arrmax(float a[], int num_elements);
+float get_arrmin(float a[], int num_elements);
+float get_aver(float a[], int num_elements);
+
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
