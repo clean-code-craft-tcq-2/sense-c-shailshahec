f9b9c33da5a6478d6dc3c1fe4f90245cb2f31ec6
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 20:12:43 +0530

    Create sense_alert.h
    sense alert header file

---
+++ sense_alert.h
@@ -0,0 +1,27 @@
+/***************************************************************************************************
+*                                                                                                  *
+*  FILENAME:                                                                                      */
+/**\file       sense_alert.h
+*
+*  \brief       Header file for selecting the senser alert
+
+*
+* FILE VERSION INFORMATION:  $Revision: $ 1.0.0
+*                            $Date:     $ 25 oct 2021
+*                            $Author:   $
+*
+* REVISION HISTORY: V1.0
+*
+***************************************************************************************************/
+
+// public global variable
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
+/**** public functions *****************************************************************************
+* Declarations of functions to be made available to other modules. Use SCOPE in front of each      *
+* line.                                                                                           */
+void emailAlerter();
+void ledAlerter();
+
+/* ################ End Of File ############################# */
