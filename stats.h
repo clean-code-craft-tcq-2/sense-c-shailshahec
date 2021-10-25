8f43e497b551de047d06c613a9060f065023269b
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 20:39:11 +0530

    Update stats.h
    Added the stats function header

--- stats.h
+++ stats.h
@@ -1,12 +1,31 @@
+/***************************************************************************************************
+*                                                                                                  *
+*  FILENAME:                                                                                      */
+/**\file       stats.h
+*
+*  \brief       Header file for stats functionality
 
+*
+* FILE VERSION INFORMATION:  $Revision: $ 1.0.0
+*                            $Date:     $ 25 oct 2021
+*                            $Author:   $
+*
+* REVISION HISTORY: V1.0
+*
+***************************************************************************************************/
+/**** typdefs/structures ***************************************************************************
+* Typedefs/structures to be made available to other modules.                                      */
+struct Stats
+{
+   float average;
+   float min;
+   float max;
+};
+
+/**** public functions *****************************************************************************
+* Declarations of functions to be made available to other modules. Use SCOPE in front of each      *
+* line.                                                                                           */
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-
-float get_arrmax(float a[], int num_elements);
-float get_arrmin(float a[], int num_elements);
-float get_aver(float a[], int num_elements);
-
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
