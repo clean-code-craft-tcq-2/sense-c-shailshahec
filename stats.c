784fa438b7aab28359a2dd82e4921ff7eac6e83c
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 19:59:16 +0530

    Update stats.c
    added check and alert function

--- stats.c
+++ stats.c
@@ -1,5 +1,9 @@
 #include "stats.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
@@ -59,5 +63,12 @@ float get_aver(float a[], int num_elements)
    return(avg);
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if(computedStats.max> maxThreshold)
+    {       
+         (*alerters[0])();
+         (*alerters[1])();
+    }
+}
+
