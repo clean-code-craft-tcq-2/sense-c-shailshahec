691d10742bb290ec246a58841d4c75944dae639b
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 21:03:12 +0530

    Update stats-test.cpp
    added the header file

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,10 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
-#include "stats.h"
-
 #include <stdlib.h>
 #include <math.h>
+#include "catch.hpp"
+#include "stats.h"
+#include "sense_alert.h"
 
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
