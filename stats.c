f096173bd060d3ed15369becb567cffdc383ae54
shailshahec <90972422+shailshahec@users.noreply.github.com>
2021-10-25 18:34:01 +0530

    Update stats.c
    Added logic for ave,min,max

--- stats.c
+++ stats.c
@@ -5,7 +5,28 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int loopcount = 0, sum = 0;
+    
+ for (loopcount = 0; loopcount < setlength; loopcount++)
+ {
+     if(numberset[loopcount] > numberset[loopcount+1])
+     {
+         s.max = numberset[loopcount];
+     }
+     if(numberset[loopcount] < numberset[loopcount+1])
+     {
+         s.min =  numberset[loopcount];
+     }
+     
+      sum = sum + numberset[loopcount];
+ }
+    
+    s.average = sum / setlength;
+    
+    return s;
 }
+
+
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
